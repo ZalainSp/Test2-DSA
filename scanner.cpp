1d20ff21c0ab2ee8f89f7f50633719bcdda48515
Zalain Spain <2023159874@ub.edu.bz>
2025-05-11 15:57:26 -0600

    added code for foldHash function

--- scanner.cpp
+++ scanner.cpp
@@ -11,7 +11,28 @@ Scanner::Scanner(int size) : tableSize(size) {
 
 int Scanner::foldHash(int key) const {
 
-    return;
+    key = abs(key); //handle negative numbers
+    string keyStr = to_string(key);
+    int sum = 0;
+    const int chunkSize = 3; //process 2 digits at a time
+
+    //dispaly folding steps
+    cout << "Folding steps for key " << key << ":\n ";
+    cout<< "Digits: "<<keyStr<<"\n";
+    cout<< "Breaking into 2 digit chunks:\n";
+
+    for(size_t i = 0; i<keyStr.length(); i+=chunkSize) {
+        int end = min(i + chunkSize, keyStr.length()); //ensure we dont go out of bounds
+        string chunk = keyStr.substr(i, end - i); //extract chunk
+        int chunkValue = stoi(chunk);//convert chunk to a integer
+        sum += chunkValue; //add chunk value to sum
+        cout<<"Chunk:"<<chunk<<"("<<chunkValue<<")\n";
+    }
+   cout<<"sum of chunks: "<<sum<<"\n";
+   //final hash value
+    int hashValue = sum % tableSize;
+    cout<< "Hash value: "<<sum<<" % "<<tableSize<<" = "<<hashValue<<"\n";
+    return hashValue; //return the final hash index
 }
 
 int Scanner::doubleHash(int key) const {
